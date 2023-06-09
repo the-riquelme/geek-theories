a6c339cdbf1a7e2070337c26153f59ab3d6e4ba0
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-06-09 18:29:47 -0300

    :sparkles: feat: implements swagger configs in gateway service

--- gateway-service/src/main/java/br/com/geek/theories/gatewayservice/configs/GatewayServiceConfig.java
+++ gateway-service/src/main/java/br/com/geek/theories/gatewayservice/configs/GatewayServiceConfig.java
@@ -1,24 +1,36 @@
 package br.com.geek.theories.gatewayservice.configs;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class GatewayServiceConfig {
 
 	@Bean
-	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p.path("/get")
-							.filters(f -> f
-								.addRequestHeader("Hello", "World")
-								.addRequestParameter("Hello", "World"))
-						.uri("http://httpbin.org:80"))
-				.route(p -> p.path("/posts-service/**")
-						.uri("lb://posts-service"))
-				.build();
+	@Lazy(false)
+	public List<GroupedOpenApi> apis(SwaggerUiConfigParameters config, RouteDefinitionLocator locator) {
+		var definitions = locator.getRouteDefinitions().collectList().block();
+		
+		definitions.stream().filter(
+      routeDefinition -> routeDefinition.getId()
+        .matches(".*-service"))
+          .forEach(routeDefinition -> {
+            String name = routeDefinition.getId();
+            config.addGroup(name);
+            GroupedOpenApi.builder()
+              .pathsToMatch("/" + name + "/**")
+              .group(name).build();
+          }
+		);
+
+		return new ArrayList<>();
 	}
   
 }
