7b7041fec76a2bbdca334e1ec7cf4d577acde290
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-06-08 22:07:48 -0300

    :bricks: ci: adds structure of discovery server

---
+++ discovery-server/src/test/java/br/com/geek/theories/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geek.theories.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
