c8f642d5f90ba889736ed2e31c565c76dfe2ad69
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-06-09 10:09:54 -0300

    :bricks: ci: adds structure of gateway server

---
+++ gateway-service/src/test/java/br/com/geek/theories/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geek.theories.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
